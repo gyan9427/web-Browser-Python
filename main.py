a9508581fe591f135f3d34771b96fc6fbbaced0d
gyan9427 <gyan9427@gmail.com>
2022-01-27 21:38:55 +0530

    Create a basic chrome frame

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+from PyQt5.QtWidgets import *
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.showMaximized()
+
+app = QApplication(sys.argv)
+QApplication.setApplicationName('My Browser')
+window = MainWindow()
+app.exec_()
+
+
+
